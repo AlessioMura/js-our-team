86de7af39451655dd00b0ac3cc7ed5cd19fb3592
AlessioMura <alessiomura03@gmail.com>
2024-10-14 17:14:14 +0200

    added member card func

--- assets/script.js
+++ assets/script.js
@@ -6,37 +6,56 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "./assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "./assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "./assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "./assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "./assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "./assets/img/female3.png"
   }
 ];
 
+
+//Member Card
+function member_card(member) {
+
+  const {name, role, email, img} = member
+
+  return `
+    <div class="card mb-3 col-4 bg-dark p-0">
+      <div class="row g-0">
+        <img src="${img}" class="img-fluid col-4" alt="...">
+          <div class="card-body col-8 text-light">
+            <h3 class="card-title">${name}</h3>
+            <div class="card-text">${role}</div>
+            <div class="card-text">${email}</div>
+          </div>
+        </div>
+      </div>
+      `
+}
